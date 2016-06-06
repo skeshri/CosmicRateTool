02d6679a71b860ac7a7284aa04baaf33e37622ce
skeshri <sumitkesri@gmail.com>
2016-06-06 15:58:38 +0200

    Update downloadFiles.sh

--- TrackAnalyzer/test/downloadFiles.sh
+++ TrackAnalyzer/test/downloadFiles.sh
@@ -4,6 +4,11 @@ DATASET=$1
 FILELIST=$2
 
 #echo "$DATASET"
+if ["$# < 2"]
+then
+echo "Enter Dataset name as first argument and second argument as filename.\n"
+exit
+fi
 
 das_client.py --limit=100000 --query="file dataset=$DATASET" >& $FILELIST
 
