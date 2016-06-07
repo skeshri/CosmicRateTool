d14984486316f4853151f1182f2f136447bd8583
skeshri <sumitkesri@gmail.com>
2016-06-07 09:20:52 +0200

    Update run.sh

--- TrackAnalyzer/test/run.sh
+++ TrackAnalyzer/test/run.sh
@@ -4,6 +4,9 @@ QUEUE=$1
 
 if [ $# -lt 1 ] 
 then
+printf "Job is submitted to Queue '8nh'.\n\n"
+printf "If you want to submit job in specific queue then please use :\n"
+printf "sh run.sh [Queue name]\n\n"
 bsub -q 8nh < job.sh 
 
 else 
