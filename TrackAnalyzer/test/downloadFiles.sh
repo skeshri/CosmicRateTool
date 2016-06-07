c02e9885a0e2fd5a66f8a95bd754c490687b0162
skeshri <sumitkesri@gmail.com>
2016-06-07 08:15:24 +0200

    Update downloadFiles.sh

--- TrackAnalyzer/test/downloadFiles.sh
+++ TrackAnalyzer/test/downloadFiles.sh
@@ -4,10 +4,11 @@ DATASET=$1
 FILELIST=$2
 
 #echo "$DATASET"
-if ["$# < 2"]
+if [ "$#" != '2' ]
 then
-echo "Enter Dataset name as first argument and second argument as filename.\n"
-exit
+  echo -e "\nNumber of argument should be two"
+  echo -e "1) Dataset name.\n2) filename.\n"
+  exit 1;
 fi
 
 das_client.py --limit=100000 --query="file dataset=$DATASET" >& $FILELIST
