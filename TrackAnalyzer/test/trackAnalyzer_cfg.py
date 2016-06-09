27a902c9a7d06459e65f70428c5ffa3d0eb84bcc
Gregor Mittag <Gregor.Mittag@cern.ch>
2016-06-09 13:38:29 +0200

    Fix cosmic-rate cfg.

--- TrackAnalyzer/test/trackAnalyzer_cfg.py
+++ TrackAnalyzer/test/trackAnalyzer_cfg.py
@@ -7,10 +7,10 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.load('Configuration.StandardSequences.MagneticField_cff') # B-field map
 process.load('Configuration.Geometry.GeometryRecoDB_cff') # Ideal geometry and interface
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff") # Global tag
-#process.GlobalTag.connect   = "frontier://FrontierProd/CMS_CONDITIONS"
- 
-#process.GlobalTag.globaltag ='80X_dataRun2_Prompt_v2' 
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff") # Global tag
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag.globaltag = GlobalTag("80X_dataRun2_Prompt_v2")
+
 process.options = cms.untracked.PSet(
      SkipEvent= cms.untracked.vstring("ProductNotFound"), # make this exception fatal
   )
@@ -24,7 +24,6 @@ readFiles = cms.untracked.vstring()
 readFiles = cms.untracked.vstring( FileUtils.loadListFromFile (os.environ['CMSSW_BASE']+'/src/CosmicRateTool/TrackAnalyzer/test/'+'list.txt') )
 process.source = cms.Source("PoolSource",
 			   fileNames = readFiles,
-			   eventsToProcess = cms.untracked.VEventRange("271861:MIN-271992:MAX")
 			   )
 
 process.demo = cms.EDAnalyzer("TrackAnalyzer",
